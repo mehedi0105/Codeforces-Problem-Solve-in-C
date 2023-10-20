b182a00641214903a75cf59f7216132700027c02
MD MEHEDI HASAN <143672978+mehedi0105@users.noreply.github.com>
2023-10-20 21:16:22 +0600

    F_Way_Too_Long_Words.c

---
+++ F_Way_Too_Long_Words.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int T;
+    scanf("%d", &T);
+    getchar();
+    while (T--)
+    {
+        char s[101];
+        scanf("%s", s);
+        int len = strlen(s);
+        if (len > 10)
+        {
+            printf("%c%d%c\n", s[0], len - 2, s[len - 1]);
+        }
+        else
+        {
+            printf("%s\n", s);
+        }
+    }
+
+    return 0;
+}
